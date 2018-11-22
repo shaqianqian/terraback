c7b60f23910b3573a9a25c3aaeda29a8444ff673
shaqianqian <sqq940814@gmail.com>
2018-11-22 15:22:16 +0100

    utilise java pour lancer python

--- python/chauffage_test.py
+++ python/chauffage_test.py
@@ -3,6 +3,6 @@ import urllib
 
 f=open("../python/chauffage_test.txt","w+")
 f.write(str(sys.argv[1]))
-print "python : l'etat de heater a change a ç"+str(sys.argv[1])
+print "python : l'etat de heater a change a "+str(sys.argv[1])
 #print sys.path
 
