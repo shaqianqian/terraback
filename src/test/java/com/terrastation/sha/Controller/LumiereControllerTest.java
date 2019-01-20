7dc4fba42832d4649edf6f8ef7d717dc6b5285e8
hazwr <hajar.bouhallaf@gmail.com>
2019-01-20 19:21:17 +0100

    Création de la classe de test LumiereControllerTest pour la classe LumiereController

---
+++ src/test/java/com/terrastation/sha/Controller/LumiereControllerTest.java
@@ -0,0 +1,11 @@
+package com.terrastation.sha.Controller;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value =LumiereController.class, secure = false)
+public class LumiereControllerTest {
+}
