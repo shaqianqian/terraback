940b9b04d0a32d3c2d0df7fe4b06ca9d9efbd13f
hazwr <hajar.bouhallaf@gmail.com>
2019-01-20 17:20:16 +0100

    PulverisationControllerTest : ajout methode setup() permettant d'initialiser le mock

--- src/test/java/com/terrastation/sha/Controller/PulverisationControllerTest.java
+++ src/test/java/com/terrastation/sha/Controller/PulverisationControllerTest.java
@@ -1,10 +1,29 @@
 package com.terrastation.sha.Controller;
 
+import com.terrastation.sha.Service.PulverisationService;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = PulverisationController.class, secure = false)
 public class PulverisationControllerTest {
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private PulverisationService terrariumService;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
 }
+
+
