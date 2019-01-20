e27bb3405d3f4bc0a8954ab76a80b358ad66aa7c
hazwr <hajar.bouhallaf@gmail.com>
2019-01-20 19:44:45 +0100

    Ajout methode setup pour LumiereControllerTest

--- src/test/java/com/terrastation/sha/Controller/LumiereControllerTest.java
+++ src/test/java/com/terrastation/sha/Controller/LumiereControllerTest.java
@@ -1,11 +1,29 @@
 package com.terrastation.sha.Controller;
 
 
+
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value =LumiereController.class, secure = false)
 public class LumiereControllerTest {
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private LumiereController terrariumService;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();   //构造MockMvc
+    }
 }
