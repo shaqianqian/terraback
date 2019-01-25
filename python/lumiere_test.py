e5b8fa7016517a5573535f19b30db93f03edc0d0
shaqianqian <sqq940814@gmail.com>
2019-01-25 11:10:38 +0100

    lumiere python

--- python/lumiere_test.py
+++ python/lumiere_test.py
@@ -1,8 +1,27 @@
 import sys
 import urllib
+import RPi.GPIO as GPIO
+import time
+
+
 
 f=open("../python/lumiere_test.txt","w+")
 f.write(str(sys.argv[1]))
 print "python : l'etat de lumiere a change a "+str(sys.argv[1])
 #print sys.path
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17,GPIO.OUT)
+if int(str(sys.argv[1]))==1:
+    print "LED on"
+    GPIO.output(17,GPIO.HIGH)
+
+else:
+    print "LED off"
+    GPIO.output(17,GPIO.LOW)
+
+    print "LED off"
+    GPIO.output(17,GPIO.LOW)
+
+
 
