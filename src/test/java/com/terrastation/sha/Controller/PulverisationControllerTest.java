5a33712ca5e0f9a473c16b21b5f8eff5ac4743c6
hazwr <hajar.bouhallaf@gmail.com>
2019-01-26 22:02:34 +0100

    PulverisationControllerTest : correctif test pour methode getMode

--- src/test/java/com/terrastation/sha/Controller/PulverisationControllerTest.java
+++ src/test/java/com/terrastation/sha/Controller/PulverisationControllerTest.java
@@ -5,7 +5,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -14,8 +16,11 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
 @RunWith(SpringRunner.class)
-@WebMvcTest(value = PulverisationController.class, secure = false)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class PulverisationControllerTest {
     @Autowired
     private WebApplicationContext context;
