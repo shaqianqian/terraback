0deda046e67a1446ddb900dfb7f5396ce55947d1
hazwr <hajar.bouhallaf@gmail.com>
2019-01-27 19:27:21 +0100

    Ajout de la classe AlarmeControllerTest pour tester le controlleur AlarmeController

--- src/main/java/com/terrastation/sha/Entity/Profil.java
+++ src/main/java/com/terrastation/sha/Entity/Profil.java
@@ -16,6 +16,19 @@ public class Profil {
 
     private String email;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
