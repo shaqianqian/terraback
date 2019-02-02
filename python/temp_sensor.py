2e9dc37a2e3000e9215c7e6db8c6c25ae2601bfe
ig1na <green_ig1na@hotmail.fr>
2019-02-03 00:57:47 +0100

    add websockets to connect raspy to vps

--- python/temp_sensor.py
+++ python/temp_sensor.py
@@ -1,28 +1,34 @@
 # -*- coding: utf-8 -*-
 
 import mysql.connector
+import asyncio
+import websockets
 from Adafruit_BME280 import *
 
-terradb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="123456",
-    database="runoob_db"
-)
+#terradb = mysql.connector.connect(
+#    host="localhost",
+#    user="root",
+#    passwd="123456",
+#    database="runoob_db"
+#)
 
-terraCursor = terradb.cursor()
+async def sendData() :
+    async with websockets.connect(
+            ws://145.239.196.28) as websocket:
 
-sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
+#terraCursor = terradb.cursor()
+        sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
+        temp = round(sensor.read_temperature(), 2)
+        humidity = round(sensor.read_humidity(), 2)
 
-degrees = round(sensor.read_temperature(), 2)
-humidity = round(sensor.read_humidity(), 2)
+        sensorVal = (humidity, temp)
+        await websocket.send(sensorVal)
 
-sensorVal = (humidity, degrees)
-insertValQuery = "INSERT INTO terrarium (create_time, humidite, temperature, update_time) VALUES (CURTIME(), %s, %s, CURTIME())"
-
-terraCursor.execute(insertValQuery, sensorVal)
-terradb.commit()
-print(terraCursor.rowcount, "record inserted.")
+asyncio.get_event_loop().run_until_complete(sensData())
+#insertValQuery = "INSERT INTO terrarium (create_time, humidite, temperature, update_time) VALUES (CURTIME(), %s, %s, CURTIME())"
+#terraCursor.execute(insertValQuery, sensorVal)
+#terradb.commit()
+#print(terraCursor.rowcount, "record inserted.")
 
 #getTempBoundsQuery = "SELECT * FROM chauffage"
 
@@ -31,5 +37,5 @@ print(terraCursor.rowcount, "record inserted.")
 #for(id, heure_debut, heure_fin, max, min, mois_debut, mois_fin) in terraCursor:
 #    print("Mois de début: {}, Mois de fin: {}, Heure de début: {}, Heure de fin: {}, Temperature min: {}, Temperature max: {}".format(mois_debut, mois_fin, heure_debut, heure_fin, min, max))
     
-terraCursor.close();
-terradb.close();
+#terraCursor.close();
+#terradb.close();
