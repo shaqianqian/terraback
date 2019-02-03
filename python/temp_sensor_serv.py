bdcd0890f80a7e4b94cca650d50ee3a2d3eb7a7b
ig1na <greenig1na@gmail.com>
2019-02-03 02:46:56 +0100

    fix bad variable call

--- python/temp_sensor_serv.py
+++ python/temp_sensor_serv.py
@@ -6,5 +6,5 @@ async def receive_data(websocket, path):
     print("{data}")
 
 start_server = websockets.serve(receive_data, 'localhost', 2681)
-asyncio.get_event_loop().run_until_complete(receive_data)
+asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
